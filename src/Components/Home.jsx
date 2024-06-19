b57ef6a06708520639e878980fdf47af6e69fa49
Mayank Paliwal <pali28913@gmail.com>
2024-06-19 19:05:01 +0530

    Completed

---
+++ src/Components/Home.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+import Header from "./Subcomponents/Header";
+import "./Home.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+export default function Home() {
+    const Dummydata=[{image:"/teammember.png",
+        name:"Vivek Tapadia",
+        designation:"ADVOCATE"
+    },
+    {image:"/teammember.png",
+        name:"Vivek Tapadia",
+        designation:"ADVOCATE"
+    },
+    {image:"/teammember.png",
+        name:"Vivek Tapadia",
+        designation:"ADVOCATE"
+    },
+    {image:"/teammember.png",
+        name:"Vivek Tapadia",
+        designation:"ADVOCATE"
+    },
+    {image:"/teammember.png",
+        name:"Vivek Tapadia",
+        designation:"ADVOCATE"
+    },
+    {image:"/teammember.png",
+        name:"Vivek Tapadia",
+        designation:"ADVOCATE"
+    },
+    {image:"/teammember.png",
+        name:"Vivek Tapadia",
+        designation:"ADVOCATE"
+    }
+
+]
+
+const OfficeData = [
+    "Madhapur",
+    "Houston, USA",
+    "Dubai, UAE",
+    "Australia",
+    "United Kingdom",
+    "Canada",
+  ];
+
+  const Services=[
+    {
+        backgroundimage:"/backgroundservices.png"
+    },
+    {
+        backgroundimage:"/property.png"
+    },
+    {
+        backgroundimage:"/backgroundservices.png"
+    },
+    {
+        backgroundimage:"/property.png"
+    },
+    {
+        backgroundimage:"/backgroundservices.png"
+    },
+  ]
+  return (
+    <div className="MainHomepage">
+      <div className="upperhero relative">
+        <img src="/herosec.png" alt="theheroimg" className="herosecimage w-full" />
+        <div className="thedivwithaboslute absolute w-full top-0 bg-transparent">
+          <Header />
+          <div className="lowertextprnt py-2 sm:py-4  md:py-8 lg:py-16 xl:py-24 container mx-auto p-6 lg:px-8">
+            <div className="innertextfprherosec w-full md:w-2/3 lg:w-3/5">
+              <p className="innerbold">IGNITE YOUR WEALTH JOURNEY WITH <span className="forotherbold">NRIS360</span>
+              </p>
+
+              <p className="lowerslimbold">Specialized Consultation for Fund Advisory, Effortless Fund Repatriation, Legal Clarity, and Accounting Excellence</p>
+              <button className='Conatct-usbutton pt-2 sm:pt-8 w-16 sm:w-2/5'>
+            <div className='flex gap-2 items-center py-1 sm:py-4 contactbtn justify-center'>
+            <p className='thecontactustext text-white'>Contact Us</p>
+            </div>
+           </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="contact-us container mx-auto  p-6 lg:px-8">
+        <div className="theuuperdiv mb-12">
+            <p className="aboutusheading">About <span className="forus">Us</span> | <span className="thegreyheadforaboutus">Our Story and 
+            Commitment</span></p>
+        </div>
+
+        <p className="theotherlowersmalltext">Our <span className="forus">Team</span></p>
+
+        <div className="theloweronefortheteammemberindialtiles pt-7 flex justify-center flex-wrap">
+            {Dummydata.map((item)=>{
+                return (
+                    <>
+                    <div className="theparentfortheteam flex flex-col justify-center items-center px-14 mt-7">
+                        <img src={item.image} alt="teamimage" className="teammemberimg pt-8 pb-4"/>
+                        <p className="teammembername pb-2">{item.name}</p>
+                        <p className="teammemberdesignation pb-8">{item.designation}</p>
+                    </div>
+                    </>
+                )
+            })}
+        </div>
+      </div>
+      <div className="ourofficesec container mx-auto  p-6 lg:px-8">
+      <p className="theotherlowersmalltext">Our <span className="forus">Office</span></p>
+
+      <div className="theloweroffieslist gap-7 pt-7 flex flex-wrap">
+      {OfficeData.map((item, index) => (
+          <div key={index} className="locationBox p-3 sm:p-5 flex items-center">
+            <div className="imageLocation">
+              <img src="/location.png" alt="location " />
+            </div>
+            <p className="forthebelowtextoflocation">{item}</p>
+          </div>
+        ))}
+      </div>
+      </div>
+      <div className="ourservices">
+        <div className="thebelowservices container mx-auto  p-6 lg:px-8">
+        <p className="theotherlowersmalltext">Our <span className="forus">Services</span></p>
+        <div className="forthebeloweservices flex flex-wrap gap-6 justify-center pt-12">
+        {Services.map((item, index) => (
+            <div className="backgroundservicesprnt">
+              <img src={item.backgroundimage} alt="services" className="servicesbg" />
+            </div>
+        ))}
+        </div>
+        </div>
+      </div>
+      <div className="whatuneedtoknow container mx-auto  p-6 lg:px-8">
+      <p className="theotherlowersmalltext pt-12 pb-12">What you <span className="forus">Need to Know</span></p>
+
+      <div className="thebelowgridimage">
+        <div className="theupperrowfortheview block lg:flex items-center gap-7">
+            <div className="harkuch"><img src="first.png" alt="firstimg" className="thefirst "/></div>
+            <div className="cbelowjedd"><img src="second.png" alt="firstimg" className="thesecond"/></div>
+        </div>
+
+        <div className="thelowerrowoftheservices block lg:flex gap-7 pt-12 ">
+            <div className="theleftsideoftherow block lg:flex gap-7">
+                <div className="theuppertop">
+                <img src="third.png" alt="firstimg" className="thethird"/>
+                </div>
+                <div className="theupperbottom">
+                <img src="forth.png" alt="firstimg" className="theforth"/>
+                </div>
+            </div>
+            <div className="therightsideoftherow">
+            <img src="fifth.png" alt="firstimg" className="thefifth"/>
+            </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/Subcomponents/Header.jsx
@@ -0,0 +1,176 @@
+import { useState } from 'react'
+import "./Header.css"
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+
+} from '@headlessui/react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeadphones } from '@fortawesome/free-solid-svg-icons'
+
+const products = [
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="">
+      <nav className="container flex mx-auto items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="/companylogo.svg" alt="" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <PopoverButton className="flex items-center gap-x-1 text-lg font-semibold leading-6 text-white">
+              Home
+              {/* <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" /> */}
+            </PopoverButton>
+
+          
+          </Popover>
+
+          <a href="#" className=" text-lg font-semibold leading-6 text-white">
+            Who we are
+          </a>
+          <a href="#" className="text-lg font-semibold leading-6 text-white">
+            Our Services
+          </a>
+          <a href="#" className="text-lg font-semibold leading-6 text-white">
+            Knowledge Bank
+          </a>
+          <a href="#" className="text-lg font-semibold leading-6 text-white">
+            Careers
+          </a>
+        </PopoverGroup>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-lg font-semibold leading-6 text-white">
+           <button className='Conatct-usbutton'>
+            <div className='flex gap-2 items-center px-6 py-3 contactbtn'>
+            <FontAwesomeIcon icon={faHeadphones} />
+            <p className='thecontactustext'>Contact Us</p>
+            </div>
+           </button>
+          </a>
+        </div>
+      </nav>
+      <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="/companylogo.svg"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <DisclosureButton className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-black hover:bg-gray-50">
+                        Home
+                      </DisclosureButton>
+                      <DisclosureButton className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-black hover:bg-gray-50">
+                        Who we are
+                      </DisclosureButton>
+                      <DisclosureButton className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-black hover:bg-gray-50">
+                        Our Services
+                      </DisclosureButton>
+                      <DisclosureButton className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-black hover:bg-gray-50">
+                        Knowledge Bank
+                      </DisclosureButton>
+                      <DisclosureButton className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-black hover:bg-gray-50">
+                        Careers
+                      </DisclosureButton>
+                    </>
+                  )}
+                </Disclosure>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                >
+                  Features
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  )
+}
